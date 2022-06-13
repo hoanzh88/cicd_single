41d4c8981d5447e23676ad2a053be12943b49752
hoanchuong <hoanchuong@ilavietnam.edu.vn>
2022-06-13 17:29:30 +0700

    cicd test add with subtract

--- test/calculatorTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-require __DIR__ . "/../calculator.php";
-
-class CalculatorTest extends \PHPUnit\Framework\TestCase{
-    public function testadd(){
-        $calculator = new \Calculator();
-        $calculator->add('a', 2);
-    }
-}
