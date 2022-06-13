76e86ef5969a35c356f18d966b985e387ddb8253
hoanchuong <hoanchuong@ilavietnam.edu.vn>
2022-06-13 17:23:39 +0700

    cicd test add with string

--- test/calculatorTest.php
+++ test/calculatorTest.php
@@ -4,6 +4,6 @@ require __DIR__ . "/../calculator.php";
 class CalculatorTest extends \PHPUnit\Framework\TestCase{
     public function testadd(){
         $calculator = new \Calculator();
-        $calculator->add(1, 2);
+        $calculator->add('a', 2);
     }
 }
